18ee7f6cbbcd6e4e06e60064a8d0009b129d8674
DavidR1997 <david.rowsellwork@gmail.com>
2018-10-22 22:41:06 +1100

    22nd October additions

---
+++ Assets/Scripts/MainRocketProjectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainRocketProjectile : MonoBehaviour
+{
+
+    public Rigidbody mainRocket;
+    public float speed;
+    public float explosionForce;
+    public ParticleSystem explosionEffect;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        Object other = collision.collider.GetComponent<Rigidbody>();
+        if (other)
+        {
+            Explode();
+            ExplosionEffect();
+        }
+
+    }
+    void Explode()
+    {
+        mainRocket.AddExplosionForce(explosionForce, transform.position, 5, 5, ForceMode.Impulse);
+        Destroy(gameObject);
+    }
+    void ExplosionEffect()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(explosionEffect, 5);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,47 +6,128 @@ public class Movement : MonoBehaviour
 {
     [Header("Movement")]
     public float mSpeed = 10f;
-    public CharacterController pController;
+    public Rigidbody pRigid;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+    public bool rotateToMainCamera = false;
+    public bool rotateWeapon = false;
+    public Rigidbody rigid;
 
-    private Vector3 moveDirection = Vector3.zero;
+    private RocketLauncher currentWeapon;
+    private Vector3 moveDir;
+    private Vector3 moveDirection;
 
     [Header("Camera")]
-    public float minY = -60f;
-    public float maxY = 60f;
+
+    public Camera pCamera;
+    public float rotX = 0f;
+    public float rotY = -60f;
+    public float sensitivity = 10f;
 
     void Awake()
     {
-        pController = GetComponent<CharacterController>();
+        pRigid = this.GetComponent<Rigidbody>();
 
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"),
-                //Obtains characters x coordinates. Upon character input, updates character coords on x axis
-                0, Input.GetAxis("Vertical"));//Does not affect Y axis, character stays grounded
-            moveDirection = transform.TransformDirection(moveDirection * mSpeed);
-            //Moves Character on x axis
-        }
-        //if (pRigid.velocity.y < 0)
-        //{
-        //    pRigid.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        //}
-        //else if (pRigid.velocity.y > 0 && !Input.GetButton("Jump"))
-        //{
-        //    pRigid.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        float inputH = Input.GetAxis("Horizontal") * mSpeed;
+        float inputV = Input.GetAxis("Vertical") * mSpeed;
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse X") * sensitivity;
 
-        //if (Input.GetKeyDown(KeyCode.Space))
+        pRigid.velocity = new Vector3(inputH * mSpeed, pRigid.velocity.y, inputV * mSpeed);
+        transform.Rotate(0, rotX, 0);
+        pCamera.transform.Rotate(rotY, 0, 0);
+        pRigid.velocity = transform.rotation * pRigid.velocity;
+
+
+        // Get the euler angles of Camera
+        Vector3 camEuler = Camera.main.transform.eulerAngles;
+
+        // Is the contorller rotating to the camera?
+        if (rotateToMainCamera)
+        {
+            // Calculate the new move direction by only taking into account the Y Axis
+            moveDir = Quaternion.AngleAxis(camEuler.y, Vector3.up) * moveDir;
+        }
+
+        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+
+        rigid.velocity = force;
+
+        Quaternion playerRotation = Quaternion.AngleAxis(camEuler.y, Vector3.up);
+        transform.rotation = playerRotation;
+
+        if (rotateWeapon)
+        {
+            Quaternion weaponRotation = Quaternion.AngleAxis(camEuler.x, Vector3.right);
+            currentWeapon.transform.localRotation = weaponRotation;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        //switch (currentState)
         //{
-        //    Jump();
+        //    case State.MouseXandY:
+        //        MouseXandY();
+        //        break;
+        //    case State.MouseX:
+        //        MouseX();
+        //        break;
+        //    case State.MouseY:
+        //        MouseY();
+        //        break;
+        //    default:
+        //        break;
+
         //}
     }
     void Jump()
     {
-
+        if (pRigid.velocity.y < 0)
+        {
+            pRigid.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (pRigid.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            pRigid.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
+    //private void MouseXandY()
+    //{
+    //    //The float for X axis is equal to Y axis + the mouse input on the X axis times our X sensitivity
+    //    float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+    //    //Y Rotation is += our mouse Y times Y sensitivity
+    //    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+    //    //Min and Max Y axis limit is clamped using Mathf.
+    //    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+    //    //Transform players local position to the new vector3 rotation. -y rotation on the X axis and X rotation on the Y axis
+    //    transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    //}
+    //private void MouseX()
+    //{
+    //    //transform rotation around Y axis by mouse input. Mouse X times sensitivityX
+    //    transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+    //}
+    //private void MouseY()
+    //{
+    //    //Rotation Y is += to mouse input for Mouse Y times Y sensitivity
+    //    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+    //    //Min and Max Y axis limit is clamped using Mathf.
+    //    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+    //    //Transform players local position to the new vector3 rotation. -y rotation on the X axis and X rotation on the Y axis
+    //    transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+    //}
 }
